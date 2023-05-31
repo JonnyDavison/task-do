e7b329bf7688f0ca27241c508a6f1a9e61d25aa4
JonnyDavison <>
2023-05-31 11:41:41 +0000

    Add task functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-   // sidenav initialization
+    // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+    
+    //datepicker init
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // Select init
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
+
+
